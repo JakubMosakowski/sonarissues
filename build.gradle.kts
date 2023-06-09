67f44999669bbe2f69e2b9c824fd9e88850ddb66
jakubmosakowski <jmosakowski@spoton.com>
2023-06-09 17:04:05 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.3.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.20")
+        classpath("org.jacoco:org.jacoco.agent:0.8.10")
+    }
+}
+
+
+apply(from = "$rootDir/jacoco/configure-jacoco.gradle.kts")
+//apply(plugin = "base-sonarqube")
